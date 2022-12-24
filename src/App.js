b5ff17a9fa2eb132da016c6c9711c717839ac80b
mohsin_ox <mohsin@oxhain.com>
2022-12-24 12:53:24 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+
+// styles File
+import "./styles/app.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/mediaquery.scss";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
